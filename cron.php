ab81e60f6b36b8d6b84f0cee5792b08a402073a2
Cedric Kastner <cedric@nur-text.de>
2012-02-07 16:42:54 +0100

    Updated examples

--- cron.php
+++ cron.php
@@ -6,25 +6,25 @@ require_once(dirname(__FILE__) . '/libs/facebook.php');
 $facebook = new Facebook(array('appId'  => '', 'secret' => ''));
 
 // Array with graph objects to fetch likes for
-$agencies =	array(array('name' => 'Nike',   'graphid' => '15087023444',     'likes' => 0),
-                  array('name' => 'Reebok', 'graphid' => '20788456835',     'likes' => 0),
-                  array('name' => 'Adidas', 'graphid' => '182162001806727', 'likes' => 0));
+$companies = array(array('name' => 'Nike',   'graphid' => '15087023444',     'likes' => 0),
+                   array('name' => 'Reebok', 'graphid' => '20788456835',     'likes' => 0),
+                   array('name' => 'Adidas', 'graphid' => '182162001806727', 'likes' => 0));
 
 // Get the likes for each entry within the array of objects
-foreach ($agencies as $key => $agency)
+foreach ($companies as $key => $company)
 {
 	// Call the graph
-	$res = $facebook->api('/' . $agency['graphid'], 'GET');
+	$res = $facebook->api('/' . $company['graphid'], 'GET');
 	
 	// Check for valid result
 	if ($res && is_array($res) && $res['likes'])
 	{
 		// Update the array with like values
-		$agencies[$key]['likes'] = $res['likes'];
+		$companies[$key]['likes'] = $res['likes'];
 	  
 	}
 	
 }
 
 // Write serialized array to cache file
-@file_put_contents(dirname(__FILE__) . '/cache.txt', serialize($agencies), LOCK_EX);
+@file_put_contents(dirname(__FILE__) . '/cache.txt', serialize($companies), LOCK_EX);
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 // Get the cached likes (which have been pulled from Facebook using cron.php)
-$agencies = @unserialize(@file_get_contents(dirname(__FILE__) . '/cache.txt'));
+$companies = @unserialize(@file_get_contents(dirname(__FILE__) . '/cache.txt'));
 
 // Fallback if cache could not be retrieved
-if (!$agencies) { require_once(dirname(__FILE__) . '/cron.php'); }
+if (!$companies) { require_once(dirname(__FILE__) . '/cron.php'); }
 
 // Debug output
 header('Content-Type: text/plain; charset=utf-8');
-print_r($agencies);
+print_r($companies);
